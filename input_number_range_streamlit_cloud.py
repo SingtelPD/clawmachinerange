25e6774118918a6b4f1824797381f0c7aa58c44d
SingtelPD <71814369+SingtelPD@users.noreply.github.com>
2022-10-17 12:20:11 +0800

    Update input_number_range_streamlit_cloud.py

--- input_number_range_streamlit_cloud.py
+++ input_number_range_streamlit_cloud.py
@@ -12,7 +12,7 @@ options = Options()
 options.headless = True
 
 
-st.title("5G Claw Machine CMS - input coupon code number range")
+st.title("🕹️📝 5G Claw Machine CMS - input game code number range")
 st.write("Please use the form below to enter the codes into the CMS system. Note: the form below accepts coupon codes that are whole numbers only.")
 st.write("If at any point you encounter an error message, this could be due to the following: 1) username and/or password is incorrrect; or 2) coupon code already exists in the CMS system.")
 
